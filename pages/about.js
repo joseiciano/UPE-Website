b545c7d53e75218069888aedfcd1637a83e50680
joseiciano <joseiciano@protonmail.com>
2021-03-09 20:04:33 -0500

    added styled components support

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  height: 80px;
+  background: #000;
+  color: #fff;
+`;
+export default class about extends Component {
+  render() {
+    return (
+      <Nav>
+        <div>Testing</div>
+      </Nav>
+    );
+  }
+}
