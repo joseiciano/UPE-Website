4f1161a68330418a49d148a5133a190e102af9d1
Taylor Gross <ttaylor.gross@gmail.com>
2021-04-06 00:39:16 -0400

    fixed styling of buttons

--- components/About.js
+++ components/About.js
@@ -1,19 +1,42 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import Image from 'next/image';
+export default class About extends Component {
+  render() {
+    return (
+      <Grid>
+        {/* Text and Buttons */}
+        <div>
+          <Header>About Us</Header>
+          <Text>Upsilon Pi Epsilon (UPE) is the largest student organization for technology at Florida International University (FIU). Our mission is to provide students with the opportunities to develop technically and professionally. We welcome students of all academic backgrounds and skill levels who are interested in technology. By joining the organization, students can participate in our programs, enjoy many benefits, and become part of the tech community at FIU!</Text>
+          <Text>UPE is supported by FIU's College of Engineering & Computing and is sponsored by top companies from all industries. Our organization has been recognized both locally and nationally, named "Outstanding Engineering Organization" by FIU and "Outstanding Chapter" by the UPE National Council.</Text>
+          <ButtonContainer>
+            <NetworkButton>Network</NetworkButton>
+            <GalleryButton>Gallery</GalleryButton>
+          </ButtonContainer>
+        </div>
+          
+        {/* Image */}
+        <ImageContainer>
+          <Image src="/About_1_3-2048x2048.png" width="500" height="500" />
+        </ImageContainer>
+    </Grid>
+    );
+  }
+  
+}
 
 const Header = styled.h1`
  text-align: center;
  color: #F7981D;
  font-weight: 900;
  font-size: 34px;
- font-family: ‘Poppins’, sans-serif;
 `;
 
 const Text = styled.p`
- margin-left:0.7rem;
- font-size: 20px;
- font-family: ‘Poppins’, sans-serif;
+  font-weight: 400;
+  margin-left:0.7rem;
+  font-size: 20px;
 `;
 
 const Grid = styled.div`
@@ -31,7 +54,7 @@ const Grid = styled.div`
     }
 `;
 
-const RightGrid = styled.div`
+const ImageContainer = styled.div`
     margin-left: 50px;
     padding: 0 20px;
     justify-content: center;
@@ -40,44 +63,41 @@ const RightGrid = styled.div`
     }
 `;
 
-const LeftGrid = styled.div`
-`;
 
-const Button = styled.button` 
+const NetworkButton = styled.button` 
     cursor: pointer;
     border: none;
     color: white;
-    margin-left: 140px;
-    margin-top: 27px;
-    padding: 12px 70px;
+    padding: 12px 60px;
     text-align: center;
-    font-weight: 900;
-    font-size: 16px;
-    font-family: ‘Poppins’, sans-serif;
-    border-top-left-radius: 50px;
-    border-bottom-left-radius: 50px;
-    border-top-right-radius: 50px;
-    border-bottom-right-radius: 50px;
+    font-weight: 800;
+    font-size: 24px;
+    border-radius: 50px;
     background: linear-gradient(90deg, rgba(253,207,12,1) 0%, rgba(248,155,33,1) 100%);
+
+`;
+const GalleryButton = styled.button` 
+    cursor: pointer;
+    border: none;
+    color: white;
+    margin-left: 10%;
+    padding: 12px 65px;
+    text-align: center;
+    font-weight: 800;
+    font-size: 24px;
+    border-radius: 50px;
+    background: linear-gradient(90deg, rgba(253,207,12,1) 0%, rgba(248,155,33,1) 100%);
+
+    @media only screen and (max-width: 550px) {
+      margin-top: 2rem;
+      margin-left: 0;
+    }
 `;
 
-export default class About extends Component {
-  render() {
-    return (
-    <Grid>
-      <LeftGrid>
-        <Header>About Us</Header>
-        <Text>Upsilon Pi Epsilon (UPE) is the largest student organization for technology at Florida International University (FIU). Our mission is to provide students with the opportunities to develop technically and professionally. We welcome students of all academic backgrounds and skill levels who are interested in technology. By joining the organization, students can participate in our programs, enjoy many benefits, and become part of the tech community at FIU!</Text>
-        <Text>UPE is supported by FIU's College of Engineering & Computing and is sponsored by top companies from all industries. Our organization has been recognized both locally and nationally, named "Outstanding Engineering Organization" by FIU and "Outstanding Chapter" by the UPE National Council.</Text>
-        <Button>Network</Button><Button>Gallery</Button>
-      </LeftGrid>
-      <RightGrid>
-        <Image src="/About_1_3-2048x2048.png" width="500" height="500" />
-      </RightGrid>
-        
-        
-        
-    </Grid>
-    );
-  }
-}
+const ButtonContainer = styled.div`
+    margin-top: 60px;
+    text-align:center;
+    @media only screen and (max-width: 550px) {
+      margin-top: 2rem;
+    }
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Name from '../components/Name'
+import About from '../components/About'
 export default function Home() {
   return (
-    <div><Name/></div>
+    <div><About/></div>
   )
 }
